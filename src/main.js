28104b96c631e5ea9396b0b9286ba0b7bd7135b7
dikih arif wibowo <dikih.wibowo@dana.id>
2022-02-19 19:05:58 +0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Home from './pages/Home.vue'
 import './index.css'
 import Router from './../routes/index'
-createApp(App)
+createApp(Home)
 .use(Router)
 .mount('#app')
